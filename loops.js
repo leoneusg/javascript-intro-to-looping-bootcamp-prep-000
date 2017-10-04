b6b73739362b855d53dd59d80d5723be767e4eab
Kendal Gates <leoneusg@yahoo.com>
2017-10-04 18:21:03 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,11 @@
 
 function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
-    return array
+  for (let i = 0; i < array.length; i++) {
   if (i === 1)  {
     console.log("I am 1 strange loop.")
   } else {
     console.log(`I am ${i} strange loops.`)
+    return array
   }
  }
 }
